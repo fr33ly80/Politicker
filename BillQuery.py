855ecae4fb9750e273ffa66b7ad1e80fff92e70e
Brendan <freely.brendan@gmail.com>
2018-10-18 16:45:39 -0400

    Update BillQuery for Bill XML Capability
    Added function to query specific BillID's and return and parse (very cursory) the XML.

--- BillQuery.py
+++ BillQuery.py
@@ -8,16 +8,21 @@ Created on Wed Oct 17 12:58:40 2018
 
 import requests as req
 import datetime
+import xml.etree.ElementTree as ElementTree
 
 API_KEY = "TevwhfGVz0YUXEHYObkR2bg9La5RpKwkN6fAg0So"
 _DEFAULT_PAGE_SIZE = "100"
 _DEFAULT_OFFSET = "0"
-_COLLECTIONS_ROOT_URI = "https://api.govinfo.gov/collections/BILLS/"
+_ROOT_URI = "https://api.govinfo.gov/"
+_BILLS_COLLECTIONS_ROOT_URI = _ROOT_URI + "/collections/BILLS/"
+_PACKAGES_ROOT_URI = _ROOT_URI + "/packages/"
 DATE_FORMAT = "%Y-%m-%dT%H:%M:%SZ"
 
 def Date(datestr):
-  datestr = datestr+"T00:00:00Z"
+  if datestr:
+    datestr = datestr+"T00:00:00Z"
   return datestr
+  #TODO: Add autoparser functionality so multiple dateformats are accepted
 
 class CongressionalBillQuery():
   def __init__(self):
@@ -26,6 +31,7 @@ class CongressionalBillQuery():
     self.offset = _DEFAULT_OFFSET
     self.start_date = None
     self.end_date = None
+    self.billID = None
 
   def _validateDate(self, date, state="start"):
     try:
@@ -67,22 +73,35 @@ class CongressionalBillQuery():
         self.offset = offset
       except:
         pass
+  
+  def setBillID(self, billID):
+    self.billID = billID
       
   def _buildURI(self, target):
     if target.lower() == "bills_collections":
-      URI = _COLLECTIONS_ROOT_URI
+      URI = _BILLS_COLLECTIONS_ROOT_URI
       URI += self.start_date + "/"
       if not isinstance(self.end_date, type(None)):
         URI += self.end_date
       URI += "?offset="+self.offset
       URI += "&pageSize="+self.page_size
-      URI += "&api_key="+self.API_KEY
-      return URI
+    elif target.lower() == "bill_summary":
+      URI = _PACKAGES_ROOT_URI
+      URI += self.billID 
+      URI += "/summary?"
+    elif target.lower() == "bill_xml":
+      URI = _PACKAGES_ROOT_URI
+      URI += self.billID 
+      URI += "/xml?"
     else:
       raise ValueError("Invalid target URI requested: %s"%target.lower())
+    URI += "&api_key="+self.API_KEY
+    return URI
   
   def queryCollections(self, startDate, endDate=None, page_size=None, 
                         offset=None):
+    startDate = Date(startDate)
+    endDate = Date(endDate)
     self.setStartDate(startDate)
     self.setEndDate(endDate)
     self.setPageSize(page_size)
@@ -90,3 +109,24 @@ class CongressionalBillQuery():
     URI = self._buildURI("bills_collections")
     response = req.get(URI)
     return response
+  
+  def queryBillSummary(self, billID):
+    self.setBillID(billID)
+    URI = self._buildURI("bill_summary")
+    response = req.get(URI)
+    return response
+  
+  def queryBillXML(self, billID):
+    self.setBillID(billID)
+    URI = self._buildURI("bill_xml")
+    response = req.get(URI)
+    return response
+  
+  def parseCollection(self, response):
+    pass #TODO: Make real
+    
+  def parseBill(self, XMLresponse):
+    #parser = ElementTree.XMLParser(encoding="utf-8")
+    root = ElementTree.fromstring(XMLresponse.content)
+    for child in root.iter('*'):
+      print(child.tag, child.attrib)
